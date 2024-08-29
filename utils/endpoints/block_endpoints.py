78054ffdd6b18cce115567c27bb2e673c2dd5adc
Gabriel <gabrieldantas8000@gmail.com>
2024-08-29 15:18:21 -0300

    Unavailable service when database update now returning

--- utils/endpoints/block_endpoints.py
+++ utils/endpoints/block_endpoints.py
@@ -1,9 +1,15 @@
 from fastapi import Request, HTTPException
+from fastapi.responses import JSONResponse
 from starlette.middleware.base import BaseHTTPMiddleware
+
 
 class BlockEndpointsMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         if request.app.state.is_updating_database:
-            raise HTTPException(status_code=503, detail="Service Unavailable: Database update in progress")
+            return JSONResponse(
+                status_code=503,
+                content={"detail": "Service Unavailable: Database update in progress"}
+            )
+
         response = await call_next(request)
         return response
