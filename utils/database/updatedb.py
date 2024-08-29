53bd9581fc5e91d043b4d3d252e1b9de9948c185
Gabriel <gabrieldantas8000@gmail.com>
2024-08-29 15:33:39 -0300

    Update database config

--- utils/database/updatedb.py
+++ utils/database/updatedb.py
@@ -1,14 +1,11 @@
-from utils.database.cleardb import clear_database
 from utils.scrap.populateDatabase import get_and_populate_establishments
 
 async def update_database(request):
     request.app.state.is_updating_database = True
     try:
-        print('Limpando registros antigos...')
-        await clear_database()
-        print('Registros antigos removidos com sucesso!')
-        print('Populando banco de dados...')
+        print('Atualizando banco de dados...')
         await get_and_populate_establishments()
-        print('Banco de dados populado com dados atualizados com sucesso!')
+        print('Banco de dados atualizado com sucesso!')
     finally:
         request.app.state.is_updating_database = False
+        print('Atualização do banco de dados finalizada')
--- utils/scrap/populateDatabase.py
+++ utils/scrap/populateDatabase.py
@@ -4,49 +4,38 @@ from prisma import Prisma
 from prisma.engine.errors import AlreadyConnectedError
 import os
 from tenacity import retry, stop_after_attempt, wait_fixed
+
 prisma = Prisma()
 
-async def insert_data(data):
-    await prisma.estabelecimento.create(
-        data={
-            'codigo_cnes': data['codigo_cnes'],
-            'numero_cnpj_entidade': data.get('numero_cnpj_entidade'),
-            'nome_razao_social': data['nome_razao_social'],
-            'nome_fantasia': data['nome_fantasia'],
-            'natureza_organizacao_entidade': data.get('natureza_organizacao_entidade'),
-            'tipo_gestao': data['tipo_gestao'],
-            'descricao_nivel_hierarquia': data.get('descricao_nivel_hierarquia'),
-            'descricao_esfera_administrativa': data['descricao_esfera_administrativa'],
-            'codigo_tipo_unidade': data['codigo_tipo_unidade'],
-            'codigo_cep_estabelecimento': data['codigo_cep_estabelecimento'],
-            'endereco_estabelecimento': data['endereco_estabelecimento'],
-            'numero_estabelecimento': data['numero_estabelecimento'],
-            'bairro_estabelecimento': data['bairro_estabelecimento'],
-            'numero_telefone_estabelecimento': data.get('numero_telefone_estabelecimento', ''),
-            'latitude_estabelecimento_decimo_grau': data['latitude_estabelecimento_decimo_grau'],
-            'longitude_estabelecimento_decimo_grau': data['longitude_estabelecimento_decimo_grau'],
-            'endereco_email_estabelecimento': data.get('endereco_email_estabelecimento', ''),
-            'numero_cnpj': data.get('numero_cnpj', ''),
-            'codigo_identificador_turno_atendimento': data['codigo_identificador_turno_atendimento'],
-            'descricao_turno_atendimento': data['descricao_turno_atendimento'],
-            'estabelecimento_faz_atendimento_ambulatorial_sus': data['estabelecimento_faz_atendimento_ambulatorial_sus'],
-            'codigo_estabelecimento_saude': data['codigo_estabelecimento_saude'],
-            'codigo_uf': data['codigo_uf'],
-            'codigo_municipio': data['codigo_municipio'],
-            'descricao_natureza_juridica_estabelecimento': data['descricao_natureza_juridica_estabelecimento'],
-            'codigo_motivo_desabilitacao_estabelecimento': data.get('codigo_motivo_desabilitacao_estabelecimento'),
-            'estabelecimento_possui_centro_cirurgico': data.get('estabelecimento_possui_centro_cirurgico', 0),
-            'estabelecimento_possui_centro_obstetrico': data.get('estabelecimento_possui_centro_obstetrico', 0),
-            'estabelecimento_possui_centro_neonatal': data.get('estabelecimento_possui_centro_neonatal', 0),
-            'estabelecimento_possui_atendimento_hospitalar': data.get('estabelecimento_possui_atendimento_hospitalar', 0),
-            'estabelecimento_possui_servico_apoio': data['estabelecimento_possui_servico_apoio'],
-            'estabelecimento_possui_atendimento_ambulatorial': data['estabelecimento_possui_atendimento_ambulatorial'],
-            'codigo_atividade_ensino_unidade': data['codigo_atividade_ensino_unidade'],
-            'codigo_natureza_organizacao_unidade': data.get('codigo_natureza_organizacao_unidade'),
-            'codigo_nivel_hierarquia_unidade': data.get('codigo_nivel_hierarquia_unidade'),
-            'codigo_esfera_administrativa_unidade': data.get('codigo_esfera_administrativa_unidade'),
-        }
+async def upsert_data(data):
+    # Verifica se o registro já existe no banco de dados
+    existing_record = await prisma.estabelecimento.find_first(
+        where={'codigo_cnes': data['codigo_cnes']}
     )
+
+    # Se existir, verifica se há diferenças nos campos
+    if existing_record:
+        update_data = {}
+        for key, value in data.items():
+            if key in existing_record and existing_record[key] != value:
+                update_data[key] = value
+
+        # Se houver dados a serem atualizados, realiza a atualização
+        if update_data:
+            await prisma.estabelecimento.update(
+                where={'codigo_cnes': data['codigo_cnes']},
+                data=update_data
+            )
+            print(f"Registro com código CNES {data['codigo_cnes']} atualizado. Dados divergentes: {update_data}")
+        else:
+            pass
+            #print(f"Registro com código CNES {data['codigo_cnes']} já está atualizado. Nenhuma divergência encontrada.")
+    else:
+        # Se não existir, insere o novo registro
+        await prisma.estabelecimento.create(
+            data=data
+        )
+        print(f"Novo registro inserido com código CNES {data['codigo_cnes']}.")
 
 def load_codes(file_path):
     codes = {}
@@ -93,10 +82,8 @@ async def get_and_populate_establishments():
                 response_data = fetch_establishments(params, headers)
                 if response_data.get('estabelecimentos'):
                     for estabelecimento in response_data['estabelecimentos']:
-                        await insert_data(estabelecimento)
+                        await upsert_data(estabelecimento)
             except requests.exceptions.RequestException as e:
                 print(f"Failed to fetch data for state {state} and unit {unit}: {e}")
 
     await prisma.disconnect()
-
-
